4aa273133634e595fe53aa467d500c2cf3217502
kerrison24 <kerrisongarcia@gmail.com>
2014-08-24 08:34:04 +1000

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+require "vendor/javascripts/bootstrap.js"
+require "vendor/javascripts/cbpScroller.js"
+require "vendor/javascripts/classie.js"
+require "vendor/javascripts/html5shiv.js"
+require "vendor/javascripts/jquery.cslider.js"
+require "vendor/javascripts/jquery.easing.1.3.js"
+require "vendor/javascripts/jquery.mixitup.js"
+require "vendor/javascripts/jquery.prettyPhoto.js"
+require "vendor/javascripts/modernizr.custom.28468.js"
+require "vendor/javascripts/respond.min.js"
+require "vendor/javascripts/script.js"
