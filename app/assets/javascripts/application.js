dfd86667b698d23eaa449dc39a871de8310ac2cc
kerrison24 <kerrisongarcia@gmail.com>
2014-08-24 21:16:59 +1000

    attempt1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,18 +12,18 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 
-//= require_relative "javascripts/bootstrap.js"
-//= require_relative "javascripts/cbpScroller.js"	
-//= require_relative "javascripts/classie.js"
-//= require_relative "javascripts/jquery.cslider.js"
-//= require_relative "javascripts/jquery.easing.1.3.js"
-//= require_relative "javascripts/jquery.mixitup.js"
-//= require_relative "javascripts/jquery.prettyPhoto.js"
-//= require_relative "javascripts/modernizr.custom.28468.js"
-//= require_relative "javascripts/respond.min.js"
-//= require_relative "javascripts/scripts.js"
+
+//= require "javascripts/bootstrap.js"
+//= require "javascripts/cbpScroller.js"	
+//= require "javascripts/classie.js"
+//= require "javascripts/jquery.cslider.js"
+//= require "javascripts/jquery.easing.1.3.js"
+//= require "javascripts/jquery.mixitup.js"
+//= require "javascripts/jquery.prettyPhoto.js"
+//= require "javascripts/modernizr.custom.28468.js"
+//= require "javascripts/respond.min.js"
+//= require "javascripts/scripts.js"
 
 //= require_tree .
 
